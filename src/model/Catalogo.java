91b934839a09dfbfbd62f4dfb56ec4c88d70b131
Romario <romario.curcio@corp.globo.com>
2016-10-06 19:43:09 -0300

    classe catalogo

---
+++ src/model/Catalogo.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.List;
+
+public class Catalogo {
+
+	private List<Produto> listaProd;
+	
+	public Catalogo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Produto> getListaProd() {
+		return listaProd;
+	}
+
+	public void setListaProd(List<Produto> listaProd) {
+		this.listaProd = listaProd;
+	}
+	
+}
